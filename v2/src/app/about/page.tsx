8e72cc16b7d233af886656e4e3508f28b22254d3
Paolo Vincent Julian <paolo.julian@accelo.com>
2025-05-10 12:08:34 +0800

    refactor: fix about me page

--- v2/src/app/about/page.tsx
+++ v2/src/app/about/page.tsx
@@ -1,10 +1,8 @@
 import CVCard from '@/app/_sections/CVCard';
 import AboutMeCard from '@/components/AboutMeCard';
 import AppBentoBox from '@/components/AppBentoBox';
-import AppTypography from '@/components/AppTypography';
 import ProfilePictureCard from '@/components/ProfilePictureCard';
 import TaglineCard from '@/components/TaglineCard';
-import Image from 'next/image';
 
 export default function AboutPage() {
   return (
@@ -12,34 +10,11 @@ export default function AboutPage() {
       {/* Desktop */}
       <div className='hidden lg:grid grid-cols-3 auto-rows-[1fr] gap-4 container'>
         {/* <!-- Row 1 --> */}
-        <AppBentoBox className='aspect-square w-full relative bg-linen'>
-          <Image
-            alt='Jhonna'
-            src='/images/grace.png'
-            fill
-            objectPosition='center'
-          />
-        </AppBentoBox>
-        <AppBentoBox className='col-span-2 relative bg-honeydew'>
-          <Image
-            alt='Jhonna'
-            src='/images/ui-ux-designer.png'
-            fill
-            objectPosition='center'
-          />
-        </AppBentoBox>
+        <ProfilePictureCard />
+        <TaglineCard className='col-span-2' />
         <AppBentoBox className='col-span-2 relative bg-neutral-100'></AppBentoBox>
         <div className='flex flex-col gap-4 h-full'>
-          <AppBentoBox className='flex flex-col gap-2 bg-linen p-6 h-1/2'>
-            <AppTypography as='h2' variant={'heading-sm'}>
-              ABOUT ME...
-            </AppTypography>
-            <AppTypography className='text-neutral-600'>
-              I am an ambitious and dedicated UI/UX Designer with a strong
-              passion for crafting intuitive and visually appealing digital
-              experiences.
-            </AppTypography>
-          </AppBentoBox>
+          <AboutMeCard />
           <CVCard />
         </div>
       </div>
